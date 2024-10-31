2958cfd7b2eaff7db2162c03b80f5d9a2a643bce
Barry Hebbron <B.D.Hebbron@tees.ac.uk>
2024-10-31 21:40:33 +0000

    More detials

---
+++ Pages/V1/Info.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorDropDown.Pages.V1
+{
+    public class InfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
